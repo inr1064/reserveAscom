0abed9546d1ef13829fd5e1f86d2198a4fe59b2b
JeonByeongIck <aseinj@gmail.com>
2018-11-14 10:41:00 +0900

    test

--- app/src/main/java/a_s/com/co/reserveascom/MainActivity.java
+++ app/src/main/java/a_s/com/co/reserveascom/MainActivity.java
@@ -1,6 +1,7 @@
 package a_s.com.co.reserveascom;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -27,5 +28,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
     }
 }
