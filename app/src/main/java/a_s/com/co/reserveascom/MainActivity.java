77f94ea3ad3641419633cf913c1bb3f4b25d7fe6
jhs910312 <jhs910312@naver.com>
2018-11-20 09:06:13 +0900

    jeong hyeonsoo 2018-11-20-09:05
    1.introduce.java
    2.MainActivity.java
    3.MyNumberPicker.java
    4.People_time.java
    5.
    main_layout.png
    6.people_time_layout.png
    7.susumu_button
    8.activity_main.xml
    9.people_time.xml
    10.styles.xml
    11.AndroidManifest.xml

--- app/src/main/java/a_s/com/co/reserveascom/MainActivity.java
+++ app/src/main/java/a_s/com/co/reserveascom/MainActivity.java
@@ -1,5 +1,6 @@
 package a_s.com.co.reserveascom;
 
+
 import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -7,28 +8,33 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+
 public class MainActivity extends AppCompatActivity {
 
     Button mainBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        super.onCreate(savedInstanceState);        setContentView(R.layout.activity_main);
 
         mainBtn = (Button) findViewById(R.id.mainBtn);
 
         mainBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,Introduce.class);
+                Intent intent = new Intent(MainActivity.this,People_time.class);
+
                 startActivity(intent);
+
             }
         });
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+
     }
+
 }
+
+
