583db790996f41dca6d730a645364f7a640f9854
CharlieJ87 <aseinj@gmail.com>
2018-11-20 14:28:17 +0900

    20181119

---
+++ app/src/main/java/a_s/com/co/reserveascom/BackPressCloseHandler.java
@@ -0,0 +1,31 @@
+package a_s.com.co.reserveascom;
+
+import android.app.Activity;
+import android.widget.Toast;
+
+public class BackPressCloseHandler {
+
+    private long backKeyPressedTime = 0;
+    private Toast toast;
+    private Activity activity;
+    public BackPressCloseHandler(Activity context){
+        this.activity = context;
+    }
+
+    public void onBackPressed(){
+        if(System.currentTimeMillis() > backKeyPressedTime + 2000){
+            backKeyPressedTime = System.currentTimeMillis();
+            showGuide();
+            return;
+        }
+        if(System.currentTimeMillis() <= backKeyPressedTime + 2000){
+            activity.finishAffinity();
+            toast.cancel();
+        }
+    }
+
+    public void showGuide(){
+        toast = Toast.makeText(activity, "push back button to exit", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
